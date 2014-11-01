d3c36dbc89db15c9e18dcfffa4d43f33f6b921b1
Yohan Boniface <yb@enix.org>
2014-11-01 17:13:47 +0100

    Add a list of suggested maps to compare with

--- front.js
+++ front.js
@@ -1,10 +1,33 @@
+var TILELAYERS = [
+    ['http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', 'OpenStreetMap'],
+    ['http://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', 'Humanitarian'],
+    ['http://{s}.tile.thunderforest.com/outdoors/{z}/{x}/{y}.png', 'Outdoors'],
+    ['http://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', 'OSM-Fr'],
+    ['http://openmapsurfer.uni-hd.de/tiles/roads/x={x}&y={y}&z={z}', 'OSM Roads'],
+    ['http://{s}.tile3.opencyclemap.org/landscape/{z}/{x}/{y}.png', 'Landscape'],
+    ['http://{s}.tile.stamen.com/toner-lite/{z}/{x}/{y}.png', 'Toner'],
+    ['http://{s}.tile2.opencyclemap.org/transport/{z}/{x}/{y}.png', 'Transport'],
+    ['http://otile1.mqcdn.com/tiles/1.0.0/osm/{z}/{x}/{y}.png', 'MapQuest Open'],
+    ['http://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', 'OpenCycleMap'],
+    ['http://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', 'Watercolor'],
+    ['http://toolserver.org/tiles/hikebike/{z}/{x}/{y}.png', 'hikebikemap'],
+    ['http://tiles.lyrk.org/ls/{z}/{x}/{y}?apikey=982c82cc765f42cf950a57de0d891076', 'Lyrk'],
+    ['http://www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', 'OSM monochrome'],
+    ['http://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', 'Hydda'],
+    ['http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', 'OpenTopoMap'],
+    ['http://{s}.tile.openstreetmap.fr/openriverboatmap/{z}/{x}/{y}.png', 'OpenRiverboatMap'],
+    ['http://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', 'OSM - Deutschland'],
+];
+
+
 L.K.Map.addInitHook(function () {
     this.whenReady(function () {
         var container = L.DomUtil.create('div', 'map-compare-container'),
             title = L.DomUtil.create('h3', '', container),
             params = {
                 tms: false,
-                url: L.K.Config.project.compareUrl,
+                url: '',
+                suggestedUrl: '',
                 active: false,
                 minZoom: this.options.minZoom,
                 maxZoom: this.options.maxZoom
@@ -15,13 +38,14 @@ L.K.Map.addInitHook(function () {
                 var container = L.DomUtil.create('div', 'map-compare', document.body);
                 container.id = 'mapCompare';
                 otherMap = L.map(container.id, {attributionControl: false});
-                tilelayer = L.tileLayer(params.url, params).addTo(otherMap);
+                tilelayer = L.tileLayer(L.K.Config.project.compareUrl, params).addTo(otherMap);
                 new L.Hash(otherMap);
             };
         var builder = new L.K.FormBuilder(params, [
             ['active', {handler: L.K.Switch, label: 'Active (ctrl+alt+C)'}],
             ['tms', {handler: L.K.Switch, label: 'TMS format.'}],
-            ['url', {handler: 'BlurInput', helpText: 'URL template.'}]
+            ['suggestedUrl', {handler: 'Select', helpText: 'Choose a map in the list…', selectOptions: TILELAYERS}],
+            ['url', {handler: 'BlurInput', helpText: '… or set a custom URL template.'}]
         ], {id: 'compare-form'});
         // TODO vertical / horizontal view
         var toggle = function () {
@@ -34,12 +58,15 @@ L.K.Map.addInitHook(function () {
                 L.DomUtil.removeClass(document.body, 'map-compare-on');
                 this.invalidateSize();
             }
-        }
+        };
+        var setUrl = function () {
+            tilelayer.setUrl(params.url || params.suggestedUrl);
+        };
         builder.on('synced', function (e) {
             if (e.field === 'active') {
                 L.bind(toggle, this)();
-            } else if (e.field === 'url' && tilelayer) {
-                tilelayer.setUrl(params.url);
+            } else if (e.field === 'url' || e.field === 'suggestedUrl') {
+                setUrl();
             }
         }, this);
         container.appendChild(builder.build());
