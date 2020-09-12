12cb9fdaee77be06966ea69fb7ea1286f15b098f
Hidde Wieringa <hidde@hiddewieringa.nl>
2020-09-12 19:46:44 +0200

    Use class syntax
    Ref https://github.com/kosmtik/kosmtik/pull/319

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-var patchConfig = function (e) {
-    e.options.compareUrl = e.project.mml.compareUrl || this.userConfig.compareUrl || 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
-};
+class Plugin {
+    constructor(config) {
+        config.addJS('/node_modules/kosmtik-map-compare/front.js');
+        config.addCSS('/node_modules/kosmtik-map-compare/front.css');
+        config.on('project:tofront', this.patchConfig);
+    }
 
-exports.Plugin = function (config) {
-    config.addJS('/node_modules/kosmtik-map-compare/front.js');
-    config.addCSS('/node_modules/kosmtik-map-compare/front.css');
-    config.on('project:tofront', patchConfig);
-};
+    patchConfig(e) {
+        e.options.compareUrl = e.project.mml.compareUrl || this.userConfig.compareUrl || 'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
+    }
+}
+
+exports = module.exports = { Plugin };
