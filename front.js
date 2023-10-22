1cdc244d6455bf88a0dcf984e903bed3db640df2
Hidde Wieringa <hidde@hiddewieringa.nl>
2023-10-22 12:34:42 +0200

    Remove Lyrk

--- front.js
+++ front.js
@@ -11,14 +11,12 @@ var TILELAYERS = [
     ['https://{s}.tile.opencyclemap.org/cycle/{z}/{x}/{y}.png', 'OpenCycleMap'],
     ['https://{s}.tile.stamen.com/watercolor/{z}/{x}/{y}.jpg', 'Watercolor'],
     ['https://toolserver.org/tiles/hikebike/{z}/{x}/{y}.png', 'hikebikemap'],
-    ['https://tiles.lyrk.org/ls/{z}/{x}/{y}?apikey=982c82cc765f42cf950a57de0d891076', 'Lyrk'],
     ['https://www.toolserver.org/tiles/bw-mapnik/{z}/{x}/{y}.png', 'OSM monochrome'],
     ['https://{s}.tile.openstreetmap.se/hydda/full/{z}/{x}/{y}.png', 'Hydda'],
     ['https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', 'OpenTopoMap'],
     ['https://{s}.tile.openstreetmap.fr/openriverboatmap/{z}/{x}/{y}.png', 'OpenRiverboatMap'],
     ['https://{s}.tile.openstreetmap.de/tiles/osmde/{z}/{x}/{y}.png', 'OSM - Deutschland']
 ];
-
 
 L.K.MapCompare = L.Evented.extend({
 
